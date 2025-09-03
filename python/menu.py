f03abfa21ebd260785e8280737babf56dcfe9794
entheryx <subhranilhazra307@gmail.com>
2025-09-03 21:43:30 +0530

    bankingUI updated

---
+++ python/menu.py
@@ -0,0 +1,69 @@
+#making the main menu for banking-system
+
+#=====================================================
+#                   MAIN MENU
+#=====================================================
+
+def mainMenu(): #mainmenu
+    print('''
+    ===========================================================
+	            	BANK MANAGEMENT SYSTEM
+    ===========================================================
+''')
+    print(''' 
+    ------------------MAIN MENU--------------------
+	[1]     USER MANAGMENT
+	[2]     ADMIN DASHBOARD
+	_____________________________________________
+	
+	[3] 	FEEDBACK
+	[4] 	CUSTOMER SUPPORT / HELP DESK ☎️ 
+	[0] 	EXIT
+''')
+    n=int(input("Choose a menu option using keyboard: "))
+    return n
+
+
+def userMenu():     #usermenu
+    print(''' 
+    ------------------USER MENU--------------------
+	[1] 	ACCOUNT HOLDER DETAILS
+	[2] 	UPDATE LOAN STATUS
+	[3] 	BALANCE ENQUIRY
+	[4] 	ACCOUNT CREATION
+	[5] 	FIXED DEPOSITS & INTEREST CALCULATOR
+	[6] 	TRANSACTIONS (Deposit, Withdraw, Transfer)
+	[7] 	TRANSACTION HISTORY
+	[8] 	LOAN SERVICES (Apply / EMI / Check Status)
+	[9] 	CHANGE PASSWORD / RESET PIN
+	[10] 	CLOSE ACCOUNT
+
+	_____________________________________________
+	
+	[11] 	FEEDBACK
+	[12] 	CUSTOMER SUPPORT / HELP DESK 
+	[0] 	EXIT
+''')
+    n=int(input("Choose a menu option using keyboard: "))
+    return n
+
+def adminMenu():     #adminmenu
+    print(''' 
+    ------------------ADMIN MENU--------------------
+	[1]     VIEW ALL ACCOUNT HOLDERS
+	[2]     VIEW FEEDBACK
+	[3]     LOAN LENDERS
+	[4]     VIEW ALL TRANSACTIONS (Daily / Monthly Reports)
+	[5]     LOAN DEFAULTERS
+	[6]     UPDATE ACCOUNT DETAILS
+        [7]     DELETE ACCOUNT (Permanent)
+	_____________________________________________
+	
+	[0] 	EXIT
+''')
+    n=int(input("Choose a menu option using keyboard: "))
+    return n
+
+mainMenu()
+userMenu()
+adminMenu()
